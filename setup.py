04556210943075a8454391698d8ee494bf82e05f
Regis Digital <regissdigital@gmail.com>
2026-06-08 10:42:34 +0530

    Bulletproof setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
-import ast
-import re
+import os
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    install_requires = [r for r in f.read().strip().split("\n") if r]
-
-_version_re = re.compile(r"__version__\s+=\s+(.*)")
-
-with open("print_formats/__init__.py", "rb") as f:
-    version = str(ast.literal_eval(_version_re.search(f.read().decode("utf-8")).group(1)))
+install_requires = []
+if os.path.exists("requirements.txt"):
+    with open("requirements.txt") as f:
+        install_requires = [r.strip() for r in f.read().strip().split("\n") if r.strip()]
 
 setup(
     name="print_formats",
-    version=version,
+    version="0.0.1",
     description="Professional Print Formats for ERPNext",
     author="Marketplace Publisher",
     author_email="hello@example.com",
